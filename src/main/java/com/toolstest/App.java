85ed5b915187ef404cde1189083fe2d50ea30e16
yc26rc2 <renan.camponez@macys.com>
2017-09-07 13:41:29 -0700

    Testing

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -1,12 +1,45 @@
 package com.toolstest;
 
-/**
- * Hello world!
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
-	private String name;
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		testIfZcCanBeShipped();
+		testIfZcCannotBeShipped();
+	}
+
+	public static void testIfZcCanBeShipped() {
+		ZipCodeRestriction zcRestriction = new ZipCodeRestriction();
+		List<ZipCodeRange> ranges = createZcRange();
+		int[] zipCodes = new int[] { 94199, 94300, 65532 };
+		for (int i = 0; i < zipCodes.length; i++) {
+			boolean result = zcRestriction.canBeShipped(ranges, zipCodes[i]);
+			System.out.println("ZipCode " + zipCodes[i] + (result ? " can" : " cannot") + " be shipped");
+		}
+	}
+
+	public static void testIfZcCannotBeShipped() {
+		ZipCodeRestriction zcRestriction = new ZipCodeRestriction();
+		List<ZipCodeRange> ranges = createZcRange();
+		int[] zipCodes = new int[] { 94133, 94650, 94230, 94600, 94299 };
+		for (int i = 0; i < zipCodes.length; i++) {
+			boolean result = zcRestriction.canBeShipped(ranges, zipCodes[i]);
+			System.out.println("ZipCode " + zipCodes[i] + (result ? " can" : " cannot") + " be shipped");
+		}
+	}
+
+	public static List<ZipCodeRange> createZcRange() {
+		ZipCodeRange range1 = new ZipCodeRange(94133, 94133);
+		ZipCodeRange range2 = new ZipCodeRange(94200, 94299);
+		ZipCodeRange range3 = new ZipCodeRange(94600, 94699);
+		List<ZipCodeRange> ranges = new ArrayList<ZipCodeRange>();
+		ranges.add(range1);
+		ranges.add(range2);
+		ranges.add(range3);
+		return ranges;
+	}
+
 }
