7ae9b68243c453fe5b0d1f932c702ccd3287ee2e
yc26rc2 <renan.camponez@macys.com>
2017-09-11 11:54:57 -0700

    Test

--- src/test/java/com/avenuecode/orders/OrdersApplicationIntegrationTest.java
+++ src/test/java/com/avenuecode/orders/OrdersApplicationIntegrationTest.java
@@ -12,9 +12,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.math.BigDecimal;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +19,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -35,7 +31,7 @@ public class OrdersApplicationIntegrationTest {
 	private MockMvc mvc;
 
 	@Test
-	public void givenEmpty_whenGetOrder_thenReturnAllOrdersJsonArray() throws Exception {
+	public void testAllOrders() throws Exception {
 
 		mvc.perform(get("/orders").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)).andDo(print())
