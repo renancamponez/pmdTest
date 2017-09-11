75ae52e7ddc40862ae0bf9a119d451f3ce8335a5
yc26rc2 <renan.camponez@macys.com>
2017-09-11 14:08:08 -0700

    Test

--- src/test/java/com/avenuecode/orders/OrdersApplicationIntegrationTest.java
+++ src/test/java/com/avenuecode/orders/OrdersApplicationIntegrationTest.java
@@ -31,7 +31,7 @@ public class OrdersApplicationIntegrationTest {
 	private MockMvc mvc;
 
 	@Test
-	public void testAllOrders() throws Exception {
+	public void testAllOrders() {
 
 		mvc.perform(get("/orders").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)).andDo(print())
