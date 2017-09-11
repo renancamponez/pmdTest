656abc4a8e49c6e2f93fda3c04f4fc931d2dd4a9
yc26rc2 <renan.camponez@macys.com>
2017-09-11 10:43:37 -0700

    Test

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -1,8 +1,8 @@
 package com.toolstest;
 
 public class App {
-  private int q = 15; // VIOLATION - Field
   public static void main( String as[] ) {  // VIOLATION - Formal
+  	private int q = 15; // VIOLATION - Field
     int r = 20 + q; // VIOLATION - Local
     for (int i = 0; i < 10; i++) { // Not a Violation (inside FOR)
       r += q;
