18f6c172f936acc43ed1fc8a458f73aa10c829ec
yc26rc2 <renan.camponez@macys.com>
2017-09-12 09:55:03 -0700

    Test

--- src/test/java/com/avenuecode/orders/OrdersApplicationIntegrationTest.java
+++ src/test/java/com/avenuecode/orders/OrdersApplicationIntegrationTest.java
@@ -44,12 +44,13 @@ public class OrdersApplicationIntegrationTest {
 
 	@Test
 	public void testFindOne() throws Exception {
-		ResponseEntity<Order> responseEntity = restTemplate.getForEntity("/orders/1", Order.class);
-		Order order = responseEntity.getBody();
-		String result = order.toString();
-		String expected = "Order(orderId=null, orderNumber=RTL_1001, discount=null, taxPercent=10.00, total=59.98, totalTax=5.99, grandTotal=65.97, status=SHIPPED, products=[Product(productId=null, upc=1257833283, sku=9394550220002, description=Diva Jeans, price=39.99), Product(productId=null, upc=1358743283, sku=7394650110003, description=Polo Shirt, price=19.99)])";
-		assertEquals(result, expected);
+		mvc.perform(get("/orders/0").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+		.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+	}
 
+	@Test
+	public void testFindNonExisting() throws Exception {
+		mvc.perform(get("/orders/0").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound());
 	}
 
 }
